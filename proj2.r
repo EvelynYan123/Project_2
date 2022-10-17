e5a728cc70a09506b6bf37cd7fc6ed93f72ad3d6
AysuIsmayilova27 <114234670+AysuIsmayilova27@users.noreply.github.com>
2022-10-17 12:27:24 +0100

    Create proj2.r

--- proj2.r
+++ proj2.r
@@ -194,6 +194,164 @@ for(i in 1:iters) {
 }
 mexp
 
-apply(mexp,2,mean)# k=1 - 10
+apply(mexp,2,mean)# k=1 - 10的时候length是多少
+
+
+
+
+
+
+
+
+Pone <- function(n, k, strategy, nreps){ 
+  N <- 2*n 
+  results = rep(0,nreps) 
+  if(strategy == 1){ 
+    for(i in 1:nreps) {
+      boxes = sample(1:N,N) 
+      prisoners = 1:N 
+      foundIt = 0 
+      for(k in prisoners) { 
+        path = c(k) 
+        tries = 1 
+        inBox = boxes[k]
+
+while(tries < n) { 
+  path = c(path, inBox) 			 			
+  if(inBox == k) { 				
+    foundIt = foundIt + 1 				
+    break; 			} 
+  else { 					
+    inBox = boxes[inBox] 			} 			
+  tries = tries+1 		} 		 		
+}
+
+
+results[i] = foundIt
+}
+}else if(strategy == 2){
+  for(i in 1:nreps) {
+  boxes = sample(1:N,N) 
+  prisoners = 1:N 
+  foundIt = 0 
+  for(k in prisoners) { 
+    path = c(sample(1:N,1)) 
+    tries = 1 
+    inBox = boxes[path]
+
+while(tries < n) { 
+  path = c(path, inBox) 			 			
+  if(inBox == k) { 				
+    foundIt = foundIt + 1 				
+    break; 			} 
+  else { 			
+    inBox = boxes[inBox] 			} 			
+  tries = tries+1 		} 		 		
+}
+results[i] = foundIt
+}
+}else if(strategy == 3){ 
+  for(i in 1:nreps) { 
+    boxes = sample(1:N,N) 
+    prisoners = 1:N 
+    foundIt = 0 
+    for(prisoner in prisoners){ 
+      path = c(sample(1:N,n)) 
+      tries = 1 
+      inbox = boxes[path] 
+      for(i in 1:n){ 
+        if(inbox[i] == prisoner){ 
+          foundIt = foundIt+1 }
+        } 
+      } 
+    results[i] = foundIt
+
+}
+}
+
+prob = sum(results)/(2n*nreps)
+
+return(prob) 
+}
+
+Pone(50,50,1, 1000) 
+Pone(50,50,2, 1000) 
+Pone(50,50,3, 1000)
+
+Pall <- function(n, strategy, nreps){ 
+  N <- 2*n 
+  results = rep(0,nreps) 
+  if(strategy == 1){ 
+    for(i in 1:nreps) { 
+    boxes = sample(1:N,N) 
+    prisoners = 1:N 
+    foundIt = 0 
+    for(k in prisoners) { 
+      path = c(k) 
+      tries = 1 
+      inBox = boxes[k]
+
+while(tries < n) { 
+  path = c(path, inBox) 			 			
+  if(inBox == k) { 				
+    foundIt = foundIt + 1 				
+    break; 			} 
+  else { 					
+    inBox = boxes[inBox] 			} 			
+  tries = tries+1 		} 		 		
+}
+
+
+results[i] = foundIt
+}
+} if(strategy == 2){ 
+  for(i in 1:nreps) { 
+  boxes = sample(1:N,N) 
+  prisoners = 1:N 
+  foundIt = 0 
+  for(k in prisoners){ 
+    path = c(sample(1:N,1)) 
+    tries = 1 
+    inBox = boxes[path]
+
+while(tries < n) { 
+  path = c(path, inBox) 			 			
+  if(inBox == k) { 				
+    foundIt = foundIt + 1 				
+    break; 			} 
+  else { 			
+    inBox = boxes[inBox] 			} 			
+  tries = tries+1 		} 		 		
+}
+results[i] = foundIt
+}
+} if(strategy == 3){ 
+  for(i in 1:nreps) { 
+    boxes = sample(1:N,N) 
+    prisoners = 1:N 
+    foundIt = 0 
+    for(k in prisoners) { 
+      path = c(sample(1:N,n))
+      tries = 1 
+      inbox = boxes[path] 
+      for(i in 1:n){ 
+        if(inbox[i] ==k){ 
+          foundIt = foundIt+1 
+          } 
+        } 
+      } 
+    results[i] = foundIt
+
+}
+}
+
+prob <- sum(results[which(results == N)])/(2n*nreps) 
+return(prob) 
+}
+
+Pall(50,1, 1000) 
+Pall(50,2, 1000) 
+Pall(50,3, 1000)
+
 
 
