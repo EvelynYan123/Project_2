f097ad94708c9988b447e0fd5cc1df874f5f11a0
Evelyn <113941619+EvelynYan123@users.noreply.github.com>
2022-10-20 18:15:19 +0100

    Update proj2.r

--- proj2.r
+++ proj2.r
@@ -1,130 +1,13 @@
 
 
-  prob <- sum(results[which(results == N)])/(2*n*nreps)
-
-set.seed(1)
-Pone <- function(n, k, strategy, nreps){  #A function to calculate the individual probability of a prisoner finding his number given number of tries, the prisoner number,the strategy and number of iterations
-  N <- 2*n #number of prisoners
-  results = rep(0,nreps)  #initialise results
-  if(strategy == 1){ #The prisoner starts at the box with their number on it, opens it and reads the number on the card: k, say. If k
-                     #is not their prisoner number, they go to box number k, open it and repeat the process until they have either
-                     #found the card with their number on it, or opened n boxes without finding it.
-    for(i in 1:nreps) {
-      boxes = sample(1:N,N) #sample boxes
-      prisoners = 1:N  #we have N =2*n prisoners
-      foundIt = 0    #initialise number of prisoners finding their number to 0
-      for(k in prisoners) {  #loop over the prisoners  
-        path = c(k)  
-        tries = 1
-        inBox = boxes[k]  #a prisoner chooses the box with his number on it
-        while(tries <= n) { #prisoner can open at most 50 boxes
-          path = c(path, inBox) 			 			
-          if(inBox == k) { 		#if the number in the box coincides with the number of a prisoner before opening more than 50 boxes he is done 		
-            foundIt = foundIt + 1 	#we count how many prsioners found their numbers			
-            break; 			} 
-          else { 		               #if the number of a prisoner does not coincide with the number in the box			
-            inBox = boxes[inBox] 			} 	#the prisoner opens the next box with the number on it equal to the card number in current box 	
-          tries = tries+1 		} 	#since the prisoner did not find his number yet we add to tries	 		
-      }
-      results[i] = foundIt 	#display overall results of prioners finding their numbers
-    }
-  }else if(strategy == 2){   
-    for(i in 1:nreps) {
-      boxes = sample(1:N,N) #sample boxes
-      prisoners = 1:N #we have N =2*n prisoners
-      foundIt = 0 #initialise number of prisoners finding their number to 0
-      for(k in prisoners) { #loop over the prisoners
-        path = c(sample(1:N,1)) #The prisoner starts with a randomly selected box
-        tries = 1 
-        inBox = boxes[path] #prisoner opens a randomly selected box
-        while(tries <= n) { #track tries of a prisoner (the prisoner should find his number before opening at most 50 boxes)
-          path = c(path, inBox)   #track the path of a prisoner			 			
-          if(inBox == k) { 	#check if the number of a card inside	of a box coincides with the prisoner's number		
-            foundIt = foundIt + 1 	#if yes add one to the number of prisoners who found their number before opening more than 50 boxes			
-            break; 			} 
-          else { 			
-            inBox = boxes[inBox] 			} 	#if not then pass this number to the next box		
-          tries = tries+1 		} 		 		#adding one to tries
-      }
-      results[i] = foundIt #keep track of all prisoners finding their numbers
-    }
-  }else if(strategy == 3){ 
-    for(i in 1:nreps) {
-      boxes = sample(1:N,N) #sample boxes
-      prisoners = 1:N #we have N = 2*n prisoners
-      foundIt = 0   #initialise number of prisoners finding their number to 0
-      for(k in prisoners) { #loop over prisoners
-        path = c(sample(1:N,n)) #sample of n randomly selected  boxes 
-        tries = 1
-        inbox = boxes[path] #prisoner opens a randomly selected box 
-        for(i in  1:n){ #loop over 50 random boxes
-          if(inbox[i] == k){  #check if the card number inside of the box coincides with the prisoner's number
-            foundIt = foundIt+1 #if yes add one to the number of prisoners who found their number before opening more than 50 boxes	
-          }
-        }
-      }
-      results[i] = foundIt #keep track of all prisoners finding their numbers
-    }  
-  }
-  prob = sum(results)/(2*n*nreps) #probability of a single prisoner succeeding in finding their number
+   ty of a single prisoner succeeding in finding their number
   return(prob)
 }
 
 
 
 
-Pall <- function(n, strategy, nreps){ #A function to calculate the probability that all prisoners find their numbers 
-                                      #given number of tries, the strategy and number of iterations
-  N <- 2*n 
-  results = rep(0,nreps)  #initialise vector of results
-  if(strategy != 3){  #for strategies except strategy 3 (that is, 1 and 2)
-    for(i in 1:nreps) {  #loop over nreps
-      boxes = sample(1:N,N) #sample boxes
-      prisoners = 1:N  #we have 2*n prisoners
-      foundIt = 0 #initialise number of prisoners who found their numbers
-      for(prisoner in prisoners) { #loop over prisoners
-        tries = 1
-        if(strategy == 1){  #consider a strategy 1
-          path = c(prisoner) #keep track of the path of a prisoner
-          inBox = boxes[prisoner] # prisoner opens a box with his number
-        }else if(strategy == 2){ #consider a strategy 2
-          path = c(sample(1:N,1)) #sample random box  
-          inBox = boxes[path]  #firstly prisoner opens a random box
-        }
-        while(tries <=n) { #prisoners can open at most 50 boxes
-          path = c(path, inBox) 		#keep track of a path	 			
-          if(inBox == prisoner) { 	#check if the number inside of the box coincides with the prisoner's number		
-            foundIt = foundIt + 1   #if yes then update foundIt
-            break; 			} 
-          else { 					
-            inBox = boxes[inBox] 			} #if no, then follow this number in the next box
-          tries = tries+1 		} 		 		#update number of tries
-      }
-      results[i] = foundIt  #how many prisoners found their numbers
-    }
-  }else{ #strategy3
-    for(i in 1:nreps) {
-  prisoners <- 1:N
-  temp <- numeric(N)
-  
-  for(j in prisoners){
-    inboxes <- sample(1:N,N)[1:n]
-    temp[j] <- match(j,inboxes)
-  }
-  temp <- na.omit(temp)
-  results[i] <- N-length(attributes(temp)$na.action)
-}
-}
 
-success_prob <- function(n,nerps){
-  for (strategy in 1:3){
-    cat('In strategy', strategy,':',fill = TRUE)
-    cat(' P(a single prisoner success)=', {Pone(n,1,strategy,nerps)},fill = TRUE)
-    cat(' P(all prisoner success)=', {Pall(n,strategy,nerps)},fill = TRUE)
-  }
-}
-success_prob(5,10000)
-success_prob(50,10000)
 
 
 
@@ -171,26 +54,4 @@ barplot(prob,ylab = 'probability',xlab = 'loop length',col = 'blue',main = 'prob
 
 
 
-
-# check
-# strategy 3----------------
-iters = 10
-results = rep(0,iters)
-
-k <- 1
-n <- 5
-N <- 2*n
-
-
-for(i in 1:iters) {
-  prisoners <- 1:N
-  temp <- numeric(N)
-  
-  for(j in prisoners){
-    inboxes <- sample(1:N,N)[1:n]
-    temp[j] <- match(j,inboxes)
-  }
-  temp <- na.omit(temp)
-  results[i] <- N-length(attributes(temp)$na.action)
-}
 
